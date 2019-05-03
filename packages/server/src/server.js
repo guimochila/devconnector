78c844107f93c7eb23cad63e320c6cff8a9a613d
Guilherme <gui.mochila@gmail.com>
2019-05-03 20:56:47 +0200

    Added middlewares + working on routes.

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -1,9 +1,19 @@
-import express from 'express';
+import cors from 'cors';
+import express, { json, urlencoded } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import userRouter from './resources/user/user.router';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API running');
-});
+// Middlwares
+app.use(helmet());
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// Routes
+app.use('/api/user', userRouter);
 
 export default app;
