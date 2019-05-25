989334adf9e0a9cc65bdb5cd8503012ed3eeabd1
Guilherme <gui.mochila@gmail.com>
2019-05-25 17:13:25 +0200

    Add jest config file and setup script.
    Add jest configuration file, easy to configure own its
    own file.
    Test-db-setup helps to setup database for testing.
    testUtils mock the req, res, next objects of express.

---
+++ packages/server/src/utils/testUtils.js
@@ -0,0 +1,26 @@
+export default function setup() {
+  const req = {
+    body: {},
+  };
+  const res = {};
+  Object.assign(res, {
+    status: jest.fn(
+      function status() {
+        return this;
+      }.bind(res),
+    ),
+    json: jest.fn(
+      function json() {
+        return this;
+      }.bind(res),
+    ),
+    send: jest.fn(
+      function send() {
+        return this;
+      }.bind(res),
+    ),
+  });
+  const next = jest.fn();
+
+  return { req, res, next };
+}
