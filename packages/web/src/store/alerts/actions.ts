53ea2aa08173df5c5db68ca583ec122b93f3f057
Guilherme <gui.mochila@gmail.com>
2019-07-01 14:30:08 +0200

    Refactored Alert Reducer

--- packages/web/src/store/alerts/actions.ts
+++ packages/web/src/store/alerts/actions.ts
@@ -1,14 +1,41 @@
-export function changeAlert(message, type, id) {
-  return function(dispatch) {
-    dispatch({
-      type: 'SET_ALERT',
+import nanoid from 'nanoid';
+import { Dispatch } from 'redux';
+import { ActionTypes } from '../types';
+export interface Alert {
+  id: string;
+  type: string;
+  message: string;
+}
+
+export interface SetAlertAction {
+  type: ActionTypes.setAlert;
+  payload: Alert;
+}
+
+export interface RemoveAlertAction {
+  type: ActionTypes.removeAlert;
+  payload: string;
+}
+
+export function setAlert(message: string, type: string) {
+  return (dispatch: Dispatch) => {
+    const id = nanoid();
+    dispatch<SetAlertAction>({
+      type: ActionTypes.setAlert,
       payload: {
-        message,
-        type,
         id,
+        type,
+        message,
       },
     });
 
-    setTimeout(() => dispatch({ type: 'REMOVE_ALERT', payload: id }), 5000);
+    setTimeout(() => dispatch<RemoveAlertAction>(removeAlert(id)), 5000);
+  };
+}
+
+export function removeAlert(id: string): RemoveAlertAction {
+  return {
+    type: ActionTypes.removeAlert,
+    payload: id,
   };
 }
--- packages/web/src/store/alerts/reducers.ts
+++ packages/web/src/store/alerts/reducers.ts
@@ -1,11 +1,12 @@
-export default function alert(state = [], action) {
-  const { type, payload } = action;
+import { Action, ActionTypes } from '../types';
+import { Alert } from './actions';
 
-  switch (type) {
-    case 'SET_ALERT':
-      return [...state, payload];
-    case 'REMOVE_ALERT':
-      return state.filter(alert => alert.id !== payload);
+export function alertReducer(state: Alert[] = [], action: Action) {
+  switch (action.type) {
+    case ActionTypes.setAlert:
+      return [...state, action.payload];
+    case ActionTypes.removeAlert:
+      return state.filter((alert: Alert) => alert.id !== action.payload);
     default:
       return state;
   }
