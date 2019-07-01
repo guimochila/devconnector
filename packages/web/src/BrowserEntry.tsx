2a5e2237da9aafb8be76d853d9c517dc9bce7fdd
Guilherme <gui.mochila@gmail.com>
2019-07-01 11:17:21 +0200

    BrowserEntry refactored

--- packages/web/src/BrowserEntry.tsx
+++ packages/web/src/BrowserEntry.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-
+import { Provider } from 'react-redux';
 import App from './components/App';
+import configureStore from './store';
 
-render(<App />, document.getElementById('app'));
+const store = configureStore();
+
+const Root = (): JSX.Element => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+render(<Root />, document.getElementById('app'));
